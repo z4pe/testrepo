a7579624b074af8cf3166196abbbb6198cdf9c77
z4pe <szalaipeter@rocketmail.com>
2020-06-10 14:42:36 +0200

    függvények paraméterek 06 06 video

--- html/main.js
+++ html/main.js
@@ -1,18 +1,20 @@
 function calcAmount() {
 let price = 1000;
 let amountInput = document.querySelector("input[name='amount-input']");
-let showAmount = document.querySelector("span.show-amount");
 let amountNumber = parseInt(amountInput.value);
 amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
 
+showSumPrice(price, amountNumber);
+}
 
-if (amountNumber > 10){
-    alert("Maximum 10 terméket vásárolhat!");
-    return;
-} else if (amountNumber < 1){
-    alert("Minimum 1 terméket vásárolhat!");
-} else {
-   let amount = parseInt(amountInput.value) * price;
-   showAmount.innerHTML = amount;
+function showSumPrice(price = 1000, amountNumber = 1){
+    let showAmount = document.querySelector("span.show-amount");
+    if (amountNumber > 10){
+        alert("Maximum 10 terméket vásárolhat!");
+    } else if (amountNumber < 1){
+        alert("Minimum 1 terméket vásárolhat!");
+    } else {
+        let amount = amountNumber * price;
+        showAmount.innerHTML = amount;
        }
 }
