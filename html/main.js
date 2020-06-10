87a8b3db94759df125ff9b0965aa55be3ee19d1c
z4pe <szalaipeter@rocketmail.com>
2020-06-10 14:07:03 +0200

    min és max feltétel hozzáadva -script egyszerűsítve

--- html/main.js
+++ html/main.js
@@ -2,7 +2,17 @@ function calcAmount() {
 let price = 1000;
 let amountInput = document.querySelector("input[name='amount-input']");
 let showAmount = document.querySelector("span.show-amount");
-let amount = parseInt(amountInput.value) * price;
+let amountNumber = parseInt(amountInput.value);
+amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
 
-showAmount.innerHTML = amount;
+
+if (amountNumber > 10){
+    alert("Maximum 10 terméket vásárolhat!");
+    return;
+} else if (amountNumber < 1){
+    alert("Minimum 1 terméket vásárolhat!");
+} else {
+   let amount = parseInt(amountInput.value) * price;
+   showAmount.innerHTML = amount;
+       }
 }
